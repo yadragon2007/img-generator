5a4931c2eedab2925250a577783068f712a97c63
yousef amr <102539850+yadragon2007@users.noreply.github.com>
2023-11-25 20:58:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const OPENAI_API_KEY = "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";
-const promptR = document.getElementById('prompt');
-const state = document.getElementById('state')
+const OPENAI_API_KEY = "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";
+const promptR = document.getElementById("prompt");
+const state = document.getElementById("state");
 const count = 5;
 
 const creat = async () => {
   //loading
-  state.innerHTML = `loading...`
+  state.innerHTML = `loading...`;
   //get img
   const img = await fetch("https://api.openai.com/v1/images/generations", {
     method: "POST",
@@ -22,9 +22,21 @@ const creat = async () => {
   });
   //change outPut to json
   const img_json = await img.json();
-  //change img src
-  const imgOutPut = document.querySelector('img')
-  imgOutPut.src = img_json.data[0].url;
+  console.log(img_json);
   //done
-  state.innerHTML = `done`
+  if (img_json.error != null) {
+    if (img_json.error.code == "content_policy_violation") {
+      state.innerHTML = `أحترم نفسك يا وسخ`;
+      state.style.color = `red`;
+    } else {
+      state.innerHTML = `failed`;
+      state.style.color = `red`;
+    }
+  } else {
+    state.innerHTML = `failed`;
+    state.style.color = `aliceblue`;
+  }
+  //change img src
+  const imgOutPut = document.querySelector("img");
+  imgOutPut.src = img_json.data[0].url;
 };
