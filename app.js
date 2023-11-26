47f4e55babc66a1a258450a4dd1a327959d0bbe0
yousef amr <102539850+yadragon2007@users.noreply.github.com>
2023-11-26 14:03:21 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+//.env
+require('dotenv').config()
+
+//set view engine
+const ejs = require("ejs");
+app.set("view engine", "ejs");
+//set static files
+app.use(express.static("public"));
+
+
+
+//requsets
+app.get("/", (req, res) => {
+  const {OPENAI_API_KEY} = process.env
+  console.log(OPENAI_API_KEY)
+  res.render("index",{
+    OPENAI_API_KEY
+  });
+});
+
+
+
+//listen 8080
+app.listen(8080, () => {
+  console.log("http://localhost:8080/");
+});
